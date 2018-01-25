2a1ae404fdccf0cd5d05fac7832c86ea5fe65660
miro <miroslav.karpis@gmail.com>
2018-01-25 13:46:11 +0100

    adding setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name='mixpanel3',
+      version='0.1.0',
+      packages=['mixpanel3'],
+      modules=['mixpanel3'],
+      description='Mixpanel API Client Python3 client.',
+      author='Miroslav Karpis',
+      author_email='miroslav.karpis@gmail.com',
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3.6',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: MIT License',
+          'Development Status :: 3 - Alpha',
+          'Topic :: Office/Business :: Data',
+      ])
